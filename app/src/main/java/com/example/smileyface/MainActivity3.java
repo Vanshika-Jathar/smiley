373ef16afa86bcfb2cfb3aae9adae272d8a42ebf
Vanshika-Jathar <vanshikahjathar@gmail.com>
2021-03-14 10:05:10 +0530

    smiley face

--- app/src/main/java/com/example/smileyface/MainActivity3.java
+++ app/src/main/java/com/example/smileyface/MainActivity3.java
@@ -5,13 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity3 extends AppCompatActivity {
-    font f;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        f = new font(MainActivity3.this);
-        setContentView(f);
+        setContentView(R.layout.activity_main3);
     }
 }
 
